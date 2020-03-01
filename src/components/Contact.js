0400409be169e38af628f3961bd46d4d2b4ce357
Joonho Han <joonhojhan@gmail.com>
2020-02-29 19:53:58 -0500

    adds functionality for contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,28 @@
 import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 
-export default function Contact() {
+export default function Contact(props) {
+	const { history } = props;
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
+	const [subject, setSubject] = useState('');
 	const [message, setMessage] = useState('');
 	const handleSubmit = e => {
 		e.preventDefault();
+		const service_id = 'default_service';
+		const template_id = 'personal_webiste';
+		const user_id = 'user_P2E9svDjgNAKJ84uNKh2H';
+		emailjs.sendForm(service_id, template_id, e.target, user_id).then(
+			result => {
+				alert(
+					'Message was sent successfully! I will get back to you as soon as I can!'
+				);
+				history.push('/');
+			},
+			error => {
+				alert("Sorry, something happened and I wasn't able to send the message...");
+			}
+		);
 	};
 
 	return (
@@ -13,11 +30,8 @@ export default function Contact() {
 			<div className="contact--header">Contact</div>
 			<form className="contact--form" onSubmit={handleSubmit}>
 				<div className="contact--field">
-					{/* <label className="contact--label" htmlFor="name">
-						Name
-					</label> */}
 					<input
-						name="name"
+						name="from_name"
 						value={name}
 						type="text"
 						placeholder="Name"
@@ -26,11 +40,8 @@ export default function Contact() {
 					/>
 				</div>
 				<div className="contact--field">
-					{/* <label className="contact--label" htmlFor="email">
-						Email
-					</label> */}
 					<input
-						name="email"
+						name="from_email"
 						value={email}
 						type="email"
 						placeholder="Email"
@@ -39,11 +50,18 @@ export default function Contact() {
 					/>
 				</div>
 				<div className="contact--field">
-					{/* <label className="contact--label" htmlFor="message">
-						Message
-					</label> */}
+					<input
+						name="subject"
+						value={subject}
+						type="text"
+						placeholder="Subject"
+						required
+						onChange={e => setSubject(e.target.value)}
+					/>
+				</div>
+				<div className="contact--field">
 					<textarea
-						name="message"
+						name="message_html"
 						value={message}
 						type="text"
 						placeholder="Message"
