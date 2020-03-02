b232ddc0fa775e539e7e5cdeca9aa805691e7d67
Joonho Han <joonhojhan@gmail.com>
2020-03-01 22:22:14 -0500

    adds images for projects

--- src/projectList.js
+++ src/projectList.js
@@ -1,7 +1,7 @@
 export default [
 	{
 		name: 'DivLab',
-		imageUrl: 'JoonhoHan2.jpeg',
+		imageUrl: 'DivLab.png',
 		description:
 			'DivLab is a full-stack web application for mocking up and building web applications. DivLab offers a sleek user interface and a great user experience with drag-and-drop integration. Users can quickly get started designing their projects without signing up. If users would like to save their progress and come back at a later time to edit their work, they can sign up and save their progress. Users are able to create multiple projects and go back to working on them at any time. Once you are finished, users are able to export an HTML file with their project to their computer and view it on the browser.',
 		technologies:
@@ -11,7 +11,7 @@ export default [
 	},
 	{
 		name: 'Food Picker',
-		imageUrl: 'JoonhoHan2.jpeg',
+		imageUrl: 'FoodPicker.png',
 		description:
 			"We've all gone through the pain of trying to decide where to eat when you're alone or with a group of friends. Food Picker is a React Native mobile app that helps you decide where to eat. Food Picker uses the Yelp API to grab data from Yelp and Google Maps to conveniently display the location of the restaurant chosen. With the help of Food Picker, you'll never have trouble deciding where to eat ever again!",
 		technologies: 'React Native, Expo, Yelp API, Google Maps',
@@ -19,7 +19,7 @@ export default [
 	},
 	{
 		name: 'Gracejammer',
-		imageUrl: 'JoonhoHan2.jpeg',
+		imageUrl: 'Gracejammer.png',
 		description:
 			'Gracejammer is a full-stack ecommerce web application for selling jam. Gracejammer can handle both logged in and guest users, manage their carts, check out and receive email confirmation for orders. It keeps records of users, products, and past orders using a Postgres database.',
 		technologies:
